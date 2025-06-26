ff8f22a232f35eb342ae132b747a17a03762e5e6
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-27 02:09:48 +0530

    Refactor main config

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
-import numpy as np
-import random
 import torch
 import os
 
@@ -21,35 +19,14 @@ def seed_torch(seed=3450):
 seed_torch()
 
 
-import argparse
 import pandas as pd
-from pprint import pprint
+from configs.config import args
 
 from run import H36MRunner, CMURunner
 from datas_dct import define_actions, define_actions_cmu
 
-parser = argparse.ArgumentParser(description='manual to this script')
-parser.add_argument('--exp_name', type=str, default="cmu", help="h36m / cmu")
-parser.add_argument('--input_n', type=int, default=10, help="")
-parser.add_argument('--output_n', type=int, default=25, help="")
-parser.add_argument('--dct_n', type=int, default=35, help="")
-parser.add_argument('--device', type=str, default="cuda:0", help="")
-parser.add_argument('--num_works', type=int, default=0)
-parser.add_argument('--test_manner', type=str, default="all", help="all / 8")
-parser.add_argument('--debug_step', type=int, default=1, help="")
-parser.add_argument('--batch-size', type=int, default=16, help="training batch size")
-parser.add_argument('--epochs', type=int, default=5000, help="number of epochs")
-parser.add_argument('--learning-rate', type=float, default=2e-4, help="initial learning rate")
-parser.add_argument('--is_train', type=bool, default='', help="")
-parser.add_argument('--is_load', type=bool, default='', help="")
-parser.add_argument('--model_path', type=str, default="", help="")
-
-parser.add_argument('--dct', type=bool, default=True, help="")
-
-args = parser.parse_args()
-
 print("\n================== Arguments =================")
-pprint(vars(args), indent=4)
+print(vars(args))
 print("==========================================\n")
 
 if args.dct == True:
